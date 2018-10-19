c549cfc22a4eb6f1e59d8a531eab3ba8bbd6cd07
Mustafa Ozhan <mustafa.ozhan@adastra.one>
2018-10-19 12:36:33 +0200

    Initial commit

---
+++ src/main/kotlin/mustafaozhan/github/com/mycurrencies/CurrencyConverterCalculatorApplication.kt
@@ -0,0 +1,11 @@
+package mustafaozhan.github.com.mycurrencies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyConverterCalculatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<CurrencyConverterCalculatorApplication>(*args)
+}
