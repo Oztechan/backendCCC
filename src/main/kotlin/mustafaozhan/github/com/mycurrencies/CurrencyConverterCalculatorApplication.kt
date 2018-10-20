3a71c286010af70a48b67073306da862256ec6c0
Mustafa Ozhan <mustafa.ozhan@adastra.one>
2018-10-20 22:28:23 +0200

    Using properties for private strings

--- src/main/kotlin/mustafaozhan/github/com/mycurrencies/CurrencyConverterCalculatorApplication.kt
+++ src/main/kotlin/mustafaozhan/github/com/mycurrencies/CurrencyConverterCalculatorApplication.kt
@@ -8,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import rx.Observable
 import rx.schedulers.Schedulers
+import java.util.*
 import java.util.concurrent.TimeUnit
+import java.io.IOException
+import java.io.FileInputStream
+import org.springframework.util.ResourceUtils
 
 
 @SpringBootApplication
@@ -16,17 +20,39 @@ class CurrencyConverterCalculatorApplication
 
 fun main(args: Array<String>) {
     runApplication<CurrencyConverterCalculatorApplication>(*args)
-    Observable.interval(1, TimeUnit.HOURS)
-            .subscribe { _ ->
-                Currencies.values().filter { it != Currencies.NULL }
+
+    val properties = Properties()
+    try {
+        val file = ResourceUtils.getFile("src/main/resources/config.properties").absolutePath
+        properties.load(FileInputStream(file))
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
+    Observable
+            .interval(1, TimeUnit.SECONDS)
+            .doOnError { throwable ->
+                throwable.printStackTrace()
+            }
+            .subscribe {
+                Currencies.values()
+                        .filter { currency ->
+                            currency != Currencies.NULL
+                        }
                         .forEach { currency ->
-                            ApiClient.get()
+                            ApiClient.get(properties.getProperty("exchange_rates_endpoint"))
                                     .create(ApiInterface::class.java)
                                     .getAllOnBase(currency)
                                     .observeOn(Schedulers.newThread())
+                                    .doOnError { throwable ->
+                                        throwable.printStackTrace()
+                                    }
                                     .subscribe { currencyResponse ->
                                         //TODO
+                                        println(it)
                                     }
+
+
                         }
             }
 }
--- src/main/kotlin/mustafaozhan/github/com/mycurrencies/rest/ApiClient.kt
+++ src/main/kotlin/mustafaozhan/github/com/mycurrencies/rest/ApiClient.kt
@@ -1,18 +1,21 @@
 package mustafaozhan.github.com.mycurrencies.rest
 
+import mustafaozhan.github.com.mycurrencies.CurrencyConverterCalculatorApplication
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
 
 /**
  * Created by Mustafa Ozhan on 2018-10-19.
  */
 class ApiClient {
     companion object {
-        fun get(): Retrofit = Retrofit.Builder()
+        fun get(baseUrl: String): Retrofit = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .baseUrl("")//baseUrl
+                .baseUrl(baseUrl)
                 .build()
     }
 }
